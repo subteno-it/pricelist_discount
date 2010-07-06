a7efb215193d99855efad3a8cbfa2d1143e5ad9f
Ysa (Open ERP) <ysa@tinyerp.co.in>
2010-07-06 18:00:10 +0530

    added pricelist_discount module

---
+++ object/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+################################################################
+#
+# pricelist_discount module for OpenERP, Pricelist Discount
+# Copyright (C) 2009 SYLEAM Info Services (<http://www.Syleam.fr/>)
+#                 {PROGRAMMER'S NAME AND E.MAIL ADDRESS GOES HERE
+#                 THERE IS AS MANY LINE AS DEVELOPPERS INVOLVED}
+#
+# This file is a part of pricelist_discount
+#
+# pricelist_discount is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pricelist_discount is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################
+
+import product_pricelist_item
+import product_pricelist
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
