683a7ce4d16ed4b81320debabd96f54998a8bc23
Sylvain Garancher <sylvain.garancher@syleam.fr>
2011-02-01 16:24:01 +0100

    [IMP] tag 0.1.1

--- __terp__.py
+++ __terp__.py
@@ -25,7 +25,7 @@
 
 {
     'name': 'Pricelist Discount',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'category': 'Custom',
     'description': """Product""",
     'author': 'SYLEAM Info Services',
