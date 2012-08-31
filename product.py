0a45fdc8cc2287cdf9a59d2b06aaf90203f041aa
Sebastien LANGE <sebastien.lange@syleam.fr>
2012-08-31 16:29:25 +0200

    [IMP] Review name of file and change for 6.1

--- product.py
+++ product.py
@@ -1,34 +1,37 @@
 # -*- coding: utf-8 -*-
-################################################################
+##############################################################################
 #
-# pricelist_discount module for OpenERP, Pricelist Discount
-# Copyright (C) 2009-20112 SYLEAM Info Services (<http://www.Syleam.fr/>)
+#    pricelist_discount module for OpenERP, Pricelist Discount
+#    Copyright (C) 2009 SYLEAM Info Services (<http://www.syleam.fr/>)
+#              Jean-Sébastien SUZANNE <jean-sebastien.suzanne@syleam.fr>
+#    Copyright (C) 2012 SYLEAM Info Services (<http://www.syleam.fr/>)
+#              Benoît MOTTIN <benoit.mottin@syleam.fr>
+#              Sebastien LANGE <sebastien.lange@syleam.fr>
 #
-# This file is a part of pricelist_discount
+#    This file is a part of pricelist_discount
 #
-# pricelist_discount is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#    pricelist_discount is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# pricelist_discount is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-# GNU General Public License for more details.
+#    pricelist_discount is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################
+##############################################################################
 
 from osv import osv
+from osv import fields
 from product._common import rounding
 from tools.translate import _
 import time
 
 class product_pricelist(osv.osv):
-
     _inherit = "product.pricelist"
 
     def price_get(self, cr, uid, ids, prod_id, qty, partner=None, context=None):
@@ -172,5 +175,23 @@ class product_pricelist(osv.osv):
         return result
 
 product_pricelist()
+
+
+class product_pricelist_item(osv.osv):
+    """
+    OpenERP Model : product.pricelist.item
+    """
+    _inherit = 'product.pricelist.item'
+
+    _columns = {
+        'discount': fields.float('Discount (%)', digits=(16, 2)),
+        }
+
+    _defaults = {
+        'discount': 0.0,
+        }
+
+product_pricelist_item()
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
